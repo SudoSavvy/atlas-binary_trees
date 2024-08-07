e672441c95635dc37ed8defff0857a6299f3c44d
SudoSavvy <donovanjheape@gmail.com>
2024-08-07 14:43:56 -0500

    Binary Trees!

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -3,7 +3,7 @@
 
 /**
  * binary_tree_node - Creates a binary tree node
- * @parent: Pointer to the parent node of the node to create (no longer used)
+ * @parent: Pointer to the parent node of the node to create (not used here)
  * @value: Value to put in the new node
  *
  * Return: Pointer to the new node, or NULL on failure
