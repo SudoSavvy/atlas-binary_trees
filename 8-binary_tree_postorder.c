04965403713d75458880bc9eecbc4679516fa9bb
SudoSavvy <donovanjheape@gmail.com>
2024-08-08 10:41:22 -0500

    Binary trees day 2

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -15,12 +15,12 @@ void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
     if (tree == NULL || func == NULL)
         return;
 
-    // Traverse the left subtree
+    /*Traverse the left subtree*/
     binary_tree_postorder(tree->left, func);
 
-    // Traverse the right subtree
+    /*Traverse the right subtree*/
     binary_tree_postorder(tree->right, func);
 
-    // Visit the root node
+    /*Visit the root node*/
     func(tree->n);
 }
