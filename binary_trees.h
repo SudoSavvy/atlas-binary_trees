1c86e45bdb18bb9e027f5d5de797d65693d1192a
SudoSavvy <donovanjheape@gmail.com>
2024-08-08 09:56:17 -0500

    Binary trees day 2

--- binary_trees.h
+++ binary_trees.h
@@ -6,12 +6,14 @@
 /**
  * struct binary_tree_s - Binary tree node
  * @n: Integer stored in the node
+ * @parent: Pointer to the parent node
  * @left: Pointer to the left child
  * @right: Pointer to the right child
  */
 typedef struct binary_tree_s
 {
     int n;
+    struct binary_tree_s *parent;
     struct binary_tree_s *left;
     struct binary_tree_s *right;
 } binary_tree_t;
